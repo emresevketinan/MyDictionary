816fce619062183ec6d2189bd6d69ef22ec7fa86
ASUS <ASUS@DESKTOP-KDFQERG>
2022-04-19 02:11:31 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> City = new MyDictionary<string>();
+            City.Add("Ankara");
+            City.Add("İstanbul");
+            City.Add("İzmir");
+
+        }
+    }
+}
